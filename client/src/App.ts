8edf783ae054c1b628d1d3f0fbe7a61d17dcd951
imaginarny <gencurpeter@gmail.com>
2025-03-05 20:09:31 +0100

    Set up kaplay canvas

--- client/src/App.ts
+++ client/src/App.ts
@@ -1,11 +1,17 @@
 import './index.css';
+import { GAME_WIDTH, GAME_HEIGHT } from "../../globals";
 import kaplay from 'kaplay'
 import { colyseusSDK } from "./core/colyseus";
 import { createLobbyScene } from './scenes/lobby';
 import type { MyRoomState } from '../../server/src/rooms/schema/MyRoomState';
 
 // Initialize kaplay
-export const k = kaplay({ background: "20252e" });
+export const k = kaplay({
+  width: GAME_WIDTH,
+  height: GAME_HEIGHT,
+  letterbox: true,
+  background: "8db7ff"
+});
 
 // Create all scenes
 createLobbyScene();
